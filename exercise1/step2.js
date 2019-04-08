aceaf8eececb5f9f3e3623c92ee358ff3b784660
Kobi Lee <kobiatlaslee@gmail.com>
2019-04-08 15:19:36 +0000

    step 2

---
+++ exercise1/step2.js
@@ -0,0 +1,32 @@
+var https = require('https');
+function getAndPrintHTML() {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  // notice that https.get takes a callback with one parameter -
+  // response, which is a Stream that represents the HTTP response
+  https.get(requestOptions, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+  var result = '';
+  response.on('data', function (data) {
+    result += data;
+  });
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+  response.on('end', function() {
+    console.log(result);
+    console.log('Response stream complete.');
+  });
+
+});
+}
+  /* Add your code here */
+getAndPrintHTML()
