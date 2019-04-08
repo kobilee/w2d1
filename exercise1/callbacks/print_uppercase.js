1b1c1f0478663c6ac3ef2707dbfb976f976912e4
Kobi Lee <kobiatlaslee@gmail.com>
2019-04-08 16:38:14 +0000

    step 6

---
+++ exercise1/callbacks/print_uppercase.js
@@ -0,0 +1,11 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printuppercase(html) {
+  console.log(html.toUpperCase());
+}
+getHTML(requestOptions, printuppercase);
